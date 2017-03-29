5956cf4a0aa55b3dedca8b03c5386f89cdc336d0
freddiefrantzen <freddie@freddiefrantzen.com>
2017-03-29 16:42:18 +0200

    Set min php version

---
+++ src/Shared/Console/CreateDatabaseCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freddie
+ * Date: 29/03/2017
+ * Time: 16:37
+ */
+
+namespace Simplex\Quickstart\Shared\Console;
+
+
+class CreateDatabaseCommand
+{
+
+}
